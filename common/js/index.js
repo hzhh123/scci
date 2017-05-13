0a9e7d066f8ac55da2329d6a7e79185977cd02f9
hzhh123 <xshzhh@sina.cn>
2017-05-13 10:15:21 +0800

    更新

---
+++ common/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+$(function() {
+    //设置侧边栏滚动条
+    $("aside").niceScroll({
+        cursorcolor:"#393d49",
+        cursoropacitymax:1,
+        touchbehavior:false,
+        cursorwidth:"6px",
+        cursorborder:"0",
+        cursorborderradius:"5px"
+    });
+
+
+})
